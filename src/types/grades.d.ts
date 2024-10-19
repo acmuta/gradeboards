508f2df6540a46e0ed13dcdd12df5b60dc61731f
Ryan Le <31494954+lryanle@users.noreply.github.com>
2024-10-18 23:40:23 -0500

    chore: updated gradeboards assets

--- src/types/grades.d.ts
+++ src/types/grades.d.ts
@@ -2,11 +2,13 @@ export interface UniversityConfig {
   university_id: string;
   university_name: string;
   university_short_name: string;
-  university_logo: string;
+  university_logo: string[];
   university_address: string;
   university_phone: string;
   university_website: string;
   student_email_domain: string;
+  primary_color: string;
+  secondary_color: string;
 }
 
 export interface UniversityConfigs {
